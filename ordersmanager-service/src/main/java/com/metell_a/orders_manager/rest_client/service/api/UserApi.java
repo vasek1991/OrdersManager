556dc89d991ef26e1f47a30a3acfc05d48c10b49
vasya <kononenko1991@gmail.com>
2016-09-20 00:01:01 +0300

    the problem was fixed

--- ordersmanager-service/src/main/java/com/metell_a/orders_manager/rest_client/service/api/UserApi.java
+++ ordersmanager-service/src/main/java/com/metell_a/orders_manager/rest_client/service/api/UserApi.java
@@ -1,5 +1,6 @@
 package com.metell_a.orders_manager.rest_client.service.api;
 
+import retrofit2.http.GET;
 import rx.Observable;
 
 import java.util.List;
@@ -8,7 +9,6 @@ import java.util.List;
  * @author Ihor Bilous
  */
 public interface UserApi {
-
     @GET("users")
     Observable<List<String>> getUsersList();
 }
--- ordersmanager-service/src/main/java/com/metell_a/orders_manager/service/ffff.java
+++ ordersmanager-service/src/main/java/com/metell_a/orders_manager/service/ffff.java
@@ -3,6 +3,6 @@ package com.metell_a.orders_manager.service;
 /**
  * Created by  on .
  */
-public class аа {
+public class ffff {
     /* some interfaces and service its realizations if need i dont know*/
 }
